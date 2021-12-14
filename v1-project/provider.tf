3aa7028d7643327cafe562a2d86b05e8fdeba8af
CT <ct@gmail.com>
2021-12-14 13:09:59 +0530

    Instance creation and running all components

--- v1-project/provider.tf
+++ v1-project/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "terraformchecks3"
-    key    = "ec321/terraform.tfstate"
+    key    = "ec-2/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform_state_lock_check"
   }
